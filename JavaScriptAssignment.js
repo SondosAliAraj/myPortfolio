7461e1283ae273b2fb91d0d42f95b119621010d3
SondosAliAraj <62075599+SondosAliAraj@users.noreply.github.com>
2023-10-04 22:15:43 +0300

    html & css & javascript assignment

---
+++ JavaScriptAssignment.js
@@ -0,0 +1,16 @@
+function findIndices(nums, target) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+
+    return [];
+}
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const result = findIndices(nums, target);
+console.log(result); 
